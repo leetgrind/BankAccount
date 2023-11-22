aed73f9a432358d422695d567d2fa2913290c20f
ace <ace@aces-MacBook-Pro.local>
2023-11-22 10:56:34 -0500

    updates

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,4 +1,12 @@
 public class BankAccount {
+
+    private double balance;
+
+    public BankAccount(double balance) {
+        System.out.println("Starting balance $" + balance);
+        this.balance = balance;
+    }
+
     public double getBalance() {
         return balance;
     }
@@ -7,16 +15,11 @@ public class BankAccount {
         this.balance = balance;
     }
 
-    private double balance;
-
-    public BankAccount(double balance) {
-        this.balance = balance;
-    }
-
     public void deposit(double amount) {
         double temp = balance;
         temp = temp + amount;
         try {
+            //artificial processing delay
             Thread.sleep(300);
         } catch (InterruptedException e) {
             System.err.println(e.getMessage());
@@ -35,12 +38,12 @@ public class BankAccount {
         double temp = balance;
         temp = balance - amount;
         try {
+            //artificial processing delay
             Thread.sleep(300);
         } catch (InterruptedException e) {
             System.err.println(e.getMessage());
         }
         System.out.println("after withdraw balance = $" + temp);
         balance = temp;
-
     }
 }
